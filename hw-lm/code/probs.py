0f48b6fd5cc34011731d1ba8b6bc9d570ef3a8e1
swadhwa5 <swadhwa5@jh.edu>
2022-10-06 21:56:01 -0400

    idek remember what I changed at this point

--- hw-lm/code/probs.py
+++ hw-lm/code/probs.py
@@ -328,7 +328,7 @@ class BackoffAddLambdaLanguageModel(AddLambdaLanguageModel):
         # # Don't forget the difference between the Wordtype z and the
         # # 1-element tuple (z,). If you're looking up counts,
         # # these will have very different counts!
-        assert self.event_count[z, ] <= self.vocab_size
+        assert self.event_count[z, ] <= self.context_count[()]
         prob_z = (self.event_count[z, ] + self.lambda_) / (self.context_count[()] + self.lambda_ * self.vocab_size)
         
         assert self.event_count[y, z] <= self.context_count[y,]
